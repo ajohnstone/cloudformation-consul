17455550714a1b37ecb11ea3b6fbd872492ba04a
Sam Johnson <sam.johnson@spongecell.com>
2014-10-30 16:59:34 -0700

    move to subdirectory

--- cf/cloud-init.sh
+++ cf/cloud-init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-chef-solo -N consul-test-chef-solo -j node.json -c /home/ubuntu/packer-chef-solo/solo.rb
+chef-solo -N consul-test-chef-solo -j /etc/chef/node.json -c /home/ubuntu/packer-chef-solo/solo.rb
 
 # All is well so signal success
 /usr/local/bin/cfn-signal -e 0 -r "Elasticsearch setup complete" 'WAIT_HANDLE'
