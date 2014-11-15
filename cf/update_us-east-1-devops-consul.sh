dd8738b28f9793231e0155a41bfc5b67bd71aa7d
Sam Johnson <sam.johnson@spongecell.com>
2014-11-14 18:10:23 -0800

    stack comes up. still need to fix consul config (it should wait for peers).

--- cf/update_us-east-1-devops-consul.sh
+++ cf/update_us-east-1-devops-consul.sh
@@ -9,9 +9,9 @@ PARAMS_FILE=./cf/$STACK_NAME.json
 
 ruby ./cf/yaml_to_json.rb ./cf/consul_formation.yml > ./cf/consul_formation.cftemplate
 
-ruby ./cf/aws_parameter_lookup.rb $VPC_NAME $REGION > ./cf/$STACK_NAME.json
+ruby ./cf/aws_parameter_lookup.rb $VPC_NAME $REGION > $PARAMS_FILE
 
-aws cloudformation create-stack \
+aws cloudformation update-stack \
 --stack-name $STACK_NAME \
 --region $REGION \
 --template-body file://cf/consul_formation.cftemplate \
