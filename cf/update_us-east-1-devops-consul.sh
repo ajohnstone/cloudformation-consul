b98d1ea5efcc7736c519be32eb8701b8d6dabc0e
Sam Johnson <sam.johnson@spongecell.com>
2014-11-14 17:44:24 -0800

    switch to yml from troposphere

---
+++ cf/update_us-east-1-devops-consul.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+REGION=us-east-1 # N. Virginia
+export AWS_DEFAULT_REGION=$REGION
+VPC_NAME=$REGION-devops
+STACK_NAME=$VPC_NAME-consul
+
+PARAMS_FILE=./cf/$STACK_NAME.json
+
+ruby ./cf/yaml_to_json.rb ./cf/consul_formation.yml > ./cf/consul_formation.cftemplate
+
+ruby ./cf/aws_parameter_lookup.rb $VPC_NAME $REGION > ./cf/$STACK_NAME.json
+
+aws cloudformation create-stack \
+--stack-name $STACK_NAME \
+--region $REGION \
+--template-body file://cf/consul_formation.cftemplate \
+--parameters file://$PARAMS_FILE
